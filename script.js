8e7b9a4fd57a7d69238ee926502e7d5e4638a99a
BEATRIZ AMARAL SANDRON <beatriz.sandron2@senaisp.edu.br>
2026-03-13 10:17:17 -0300

    ex4

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const botao = document.getElementById("btnAdicionar");
-botao.addEventListener("click", function(){
-    let texto= document.getElementById("inputItem").value;
-    let li = document.createElement("li");
-    li.textContent = texto;
-    document.getElementById("lista").appendChild(li);
-    
+let inputTarefa = document.getElementById("inputTarefa");
+let btnAdicionar = document.querySelector("#btnAdicionar");
+let listaTarefas = document.querySelector("#listaTarefas");
+let mensagem = document.querySelector("#mensagem");
+
+btnAdicionar.addEventListener("click", () => {
+
+let texto = inputTarefa.value;
+if(texto === ""){
+
+mensagem.innerText = "Tarefa vazia!";
+mensagem.className = "text-danger fw-bold";
+
+return;
+}
+
+let li = document.createElement("li");
+
+li.innerText = texto;
+li.className = "list-group-item";
+listaTarefas.appendChild(li);
+inputTarefa.value = "";
+mensagem.innerText = "Tarefa adicionada!";
+mensagem.className = "text-success fw-bold";
 })
