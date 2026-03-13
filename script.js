df0116bf635dd67b91dc27a249119e750020b816
BEATRIZ AMARAL SANDRON <beatriz.sandron2@senaisp.edu.br>
2026-03-13 10:31:33 -0300

    ex6

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 const botao = document.getElementById("btnAdicionar");
+
 botao.addEventListener("click", function(){
 
 let tarefa = document.getElementById("inputTarefa").value;
-let li = document.createElement("li");
-li.className = "list-group-item";
 
-// checkbox
+let li = document.createElement("li");
+
+li.className = "list-group-item d-flex justify-content-between align-items-center";
+
+// lado esquerdo
+let divEsquerda = document.createElement("div");
+
 let checkbox = document.createElement("input");
 checkbox.type = "checkbox";
 checkbox.className = "form-check-input me-2";
 
-// texto
 let span = document.createElement("span");
 span.textContent = tarefa;
 
-// botão remover
-let botaoRemover = document.createElement("button");
-botaoRemover.textContent = "Remover";
-botaoRemover.className = "btn btn-danger btn-sm ms-3";
-
-// riscar tarefa
 checkbox.addEventListener("change", function(){
 
 if(checkbox.checked){
 span.style.textDecoration = "line-through";
-
 }else{
 span.style.textDecoration = "none";
 }
 
 });
-// remover tarefa
+
+divEsquerda.appendChild(checkbox);
+divEsquerda.appendChild(span);
+
+// botão remover
+let botaoRemover = document.createElement("button");
+botaoRemover.textContent = "Remover";
+botaoRemover.className = "btn btn-danger btn-sm";
+
 botaoRemover.addEventListener("click", function(){
+
 li.remove();
 
 });
-li.appendChild(checkbox);
-li.appendChild(span);
+
+li.appendChild(divEsquerda);
 li.appendChild(botaoRemover);
+
 document.getElementById("listaTarefas").appendChild(li);
 
 });
