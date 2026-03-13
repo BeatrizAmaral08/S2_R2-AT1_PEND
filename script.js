72e79a40c7b3240be25687bf6d39707ce944c47d
BEATRIZ AMARAL SANDRON <beatriz.sandron2@senaisp.edu.br>
2026-03-13 09:40:36 -0300

    ex2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const botao= document.getElementById("botao");
+const botao = document.getElementById("botao");
 botao.addEventListener("click", function(){
     let texto= document.getElementById("texto");
-    texto.textContent = "texto alterado"
+    texto.textContent = "texto alterado com javaScript"
 })
