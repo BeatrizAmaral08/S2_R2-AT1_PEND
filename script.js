4937327665247220bf08a3921484b0d8cb617cd3
BEATRIZ AMARAL SANDRON <beatriz.sandron2@senaisp.edu.br>
2026-03-13 10:27:53 -0300

    ex6

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 const botao = document.getElementById("btnAdicionar");
-
 botao.addEventListener("click", function(){
 
 let tarefa = document.getElementById("inputTarefa").value;
-
 let li = document.createElement("li");
+li.className = "list-group-item";
 
-li.className = "list-group-item d-flex justify-content-between";
+// checkbox
+let checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+checkbox.className = "form-check-input me-2";
 
+// texto
 let span = document.createElement("span");
-
 span.textContent = tarefa;
 
+// botão remover
 let botaoRemover = document.createElement("button");
-
 botaoRemover.textContent = "Remover";
+botaoRemover.className = "btn btn-danger btn-sm ms-3";
 
-botaoRemover.className = "btn btn-danger btn-sm";
+// riscar tarefa
+checkbox.addEventListener("change", function(){
 
+if(checkbox.checked){
+span.style.textDecoration = "line-through";
+
+}else{
+span.style.textDecoration = "none";
+}
+
+});
+// remover tarefa
 botaoRemover.addEventListener("click", function(){
-
 li.remove();
 
 });
-
+li.appendChild(checkbox);
 li.appendChild(span);
-
 li.appendChild(botaoRemover);
-
 document.getElementById("listaTarefas").appendChild(li);
 
 });
