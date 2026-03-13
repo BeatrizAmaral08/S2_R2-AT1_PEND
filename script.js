bfa8499f00fbc4638248dcee9d0f7304e0104849
BEATRIZ AMARAL SANDRON <beatriz.sandron2@senaisp.edu.br>
2026-03-13 09:31:00 -0300

    ex2

---
+++ script.js
@@ -0,0 +1,5 @@
+const botao= document.getElementById("botao");
+botao.addEventListener("click", function(){
+    let texto= document.getElementById("texto");
+    texto.textContent = "texto alterado"
+})
