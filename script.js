d0c4f828506d0b6cb524b27c4dfdc5ac32b8ebe9
BEATRIZ AMARAL SANDRON <beatriz.sandron2@senaisp.edu.br>
2026-03-13 10:28:48 -0300

    ex5

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 const botao = document.getElementById("btnAdicionar");
-
 botao.addEventListener("click", function(){
 
 let tarefa = document.getElementById("inputTarefa").value;
-
 let li = document.createElement("li");
-
 li.className = "list-group-item d-flex justify-content-between";
 
 let span = document.createElement("span");
-
 span.textContent = tarefa;
 
 let botaoRemover = document.createElement("button");
-
 botaoRemover.textContent = "Remover";
-
 botaoRemover.className = "btn btn-danger btn-sm";
-
 botaoRemover.addEventListener("click", function(){
-
 li.remove();
 
 });
