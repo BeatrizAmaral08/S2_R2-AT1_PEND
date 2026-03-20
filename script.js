d71ca3b1e6774b7f90dc7dc78783b5aaba3585e4
BEATRIZ AMARAL SANDRON <bisandron@gmail.com>
2026-03-20 09:10:05 -0300

    ex3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const botao = document.getElementById("btnAdicionar");
-botao.addEventListener("click", function(){
+botao.addEventListener("click", function(){ // adiciona click ao botão
     let texto= document.getElementById("inputItem").value;
-    let li = document.createElement("li");
+    let li = document.createElement("li"); // cria um novo item na lista
     li.textContent = texto;
     document.getElementById("lista").appendChild(li);
     
