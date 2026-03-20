d1ff70d0ebefd3b8be9069c1b2fdf5bccf8252c2
BEATRIZ AMARAL SANDRON <bisandron@gmail.com>
2026-03-20 08:46:10 -0300

    ex5

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const botao = document.getElementById("btnAdicionar");
-botao.addEventListener("click", function(){
+botao.addEventListener("click", function(){ // adiciona um click no botão
 
 let tarefa = document.getElementById("inputTarefa").value;
-let li = document.createElement("li");
+let li = document.createElement("li"); // cria um novo item na lista
 li.className = "list-group-item d-flex justify-content-between";
 
-let span = document.createElement("span");
+let span = document.createElement("span"); // armazena o texto da tarefa digitada
 span.textContent = tarefa;
 
-let botaoRemover = document.createElement("button");
+let botaoRemover = document.createElement("button"); //cria o botão remover
 botaoRemover.textContent = "Remover";
 botaoRemover.className = "btn btn-danger btn-sm";
 botaoRemover.addEventListener("click", function(){
