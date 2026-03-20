5ea704bb7591d15d1aeedf00b56cf3334b904e68
BEATRIZ AMARAL SANDRON <bisandron@gmail.com>
2026-03-20 09:11:52 -0300

    ex2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botao = document.getElementById("botao");
-botao.addEventListener("click", function(){
+botao.addEventListener("click", function(){ // adiciona click no botão
     let texto= document.getElementById("texto");
-    texto.textContent = "texto alterado com javaScript"
+    texto.textContent = "texto alterado com javaScript" // Altera o conteúdo de texto do elemento
 })
