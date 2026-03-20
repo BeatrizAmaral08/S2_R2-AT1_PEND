c473bc9e48513749f159052170002a20a0579ee3
BEATRIZ AMARAL SANDRON <bisandron@gmail.com>
2026-03-20 09:14:02 -0300

    ex1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const botao = document.getElementById("botao");
-botao.addEventListener("click", function(){
-const nome = document.getElementById("nome").value;
-const mensagem = document.getElementById("mensagem");
-mensagem.textContent = "Olá " + nome +"! Seja bem vindo(a)";
+botao.addEventListener("click", function(){ // adiciona click no botão
+const nome = document.getElementById("nome").value; // Pega o valor digitado no input "nome"
+const mensagem = document.getElementById("mensagem"); 
+mensagem.textContent = "Olá " + nome +"! Seja bem vindo(a)";  // Define o texto da mensagem com o nome digitado
 });
